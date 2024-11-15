db5444a3e216df90892bf72bc6a63374c0b041ff
Brenda <bren.itzel14@gmail.com>
2024-11-14 20:50:11 -0600

    PRIMERO

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppPrueba1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
