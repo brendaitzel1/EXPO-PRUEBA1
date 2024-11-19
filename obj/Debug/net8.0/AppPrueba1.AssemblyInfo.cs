7d9dfd400039bf08e7b2838ad6caedcd2c3fd7f6
Brenda <bren.itzel14@gmail.com>
2024-11-18 21:16:26 -0600

    Revert "SALUDO"
    This reverts commit 0bfce573e7125d41ad19bc131b373bf1373d8287.

--- obj/Debug/net8.0/AppPrueba1.AssemblyInfo.cs
+++ obj/Debug/net8.0/AppPrueba1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppPrueba1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1540f9e2b6e62c3c9a9c24129b9875aae5ea2b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppPrueba1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppPrueba1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
